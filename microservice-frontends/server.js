0927fa969ae8c38168b48a1cbe5074b422b96355
Jake Loynes <development@ljmuk.co.uk>
2025-09-15 19:08:19 +0100

    Fix server.js - restore microservice testing dashboard routes

--- microservice-frontends/server.js
+++ microservice-frontends/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -27,33 +28,15 @@ app.get('/health', (req, res) => {
 });
 
 const services = ['settings', 'inventory', 'sales', 'purchases', 'ebay', 'vinted', 'reporting', 'accounting', 'ad-generator', 'rules-engine', 'auto-buying', 'media', 'email-ingest', 'status'];
-
 services.forEach(service => {
     app.get('/test/' + service, (req, res) => {
         res.sendFile(path.join(__dirname, 'test-' + service + '.html'));
     });
 });
 
-app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'test.html'));
-});
-
-app.get('/purchases-test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'purchases-test.html'));
-});
-
-app.get('/purchases', (req, res) => {
-    res.sendFile(path.join(__dirname, 'purchases.html'));
-});
-
-app.get('/diagnostic-overlay.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'diagnostic-overlay.html'));
-});
-
 app.listen(PORT, '0.0.0.0', () => {
     console.log('MICROSERVICE TESTING DASHBOARD running on port ' + PORT);
     console.log('Main route: / -> dashboard.html');
-    console.log('Test routes: /test/{service-name}');
 });
 
 module.exports = app;
