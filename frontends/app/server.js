fba9beb15ec36bfcce982a73818830696364bda2
Jake Loynes <development@ljmuk.co.uk>
2025-10-29 12:45:39 +0000

    Fix CSP in Next.js server to allow eBay OAuth service connections

--- frontends/app/server.js
+++ frontends/app/server.js
@@ -13,6 +13,16 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
+      // Set CSP headers to allow OAuth service
+      res.setHeader('Content-Security-Policy', 
+        "default-src 'self'; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+        "img-src 'self' data: https:; " +
+        "connect-src 'self' https://autorestock-user-service-production.up.railway.app https://delightful-liberation-production.up.railway.app https://*.up.railway.app; " +
+        "font-src 'self' data:;"
+      );
+      
       const parsedUrl = parse(req.url, true);
       await handle(req, res, parsedUrl);
     } catch (err) {
@@ -29,6 +39,7 @@ app.prepare().then(() => {
     console.log(`> Ready on http://${hostname}:${port}`);
   });
 });
+
 
 
 
